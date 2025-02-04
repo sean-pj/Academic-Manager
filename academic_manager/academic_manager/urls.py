69638099b8a0bf8c1bb36738cea2ce6faebab549
Sean Murphy <sean.peterjames.murphy@gmail.com>
2025-02-04 18:03:57 -0500

    feat: added student and teacher view paths

--- academic_manager/academic_manager/urls.py
+++ academic_manager/academic_manager/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("core.urls"))
+    path('', include("core.urls")),
+    path('teachers/', include("teachers.urls")),
+    path('students/', include("students.urls"))
 ]
