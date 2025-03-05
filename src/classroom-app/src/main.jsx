6d9aeb2a20b6f0cbc53e43c742e47fd67d7c2d9d
Sean Murphy <sean.peterjames.murphy@gmail.com>
2025-03-04 19:42:23 -0500

    feat: added home page and student dashboard routing

--- src/classroom-app/src/main.jsx
+++ src/classroom-app/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import StudentDashboard from './StudentDashboard.jsx';
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/student" ,
+    element: <StudentDashboard></StudentDashboard>,
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
