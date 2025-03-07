2f22548620362ba7269e1f2dfb217cd94937dc7e
nathanquinnkwok <nathanyk@my.yorku.ca>
2025-03-06 20:45:46 -0500

    feat: Update assignment - models.py
    Updated the models.py for the assignment to include the basic variables

--- src/academic_manager/assignment/models.py
+++ src/academic_manager/assignment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Assignment(models.Model):
+    # Store the variables for this class
+    title = models.CharField(max_length=255) # Name of Assignment
+    description = models.TextField() # Description of Assignment
+    due_date = models.DateTimeField() # Store the due date
+
+    def __str__(self):
+        return self.name
