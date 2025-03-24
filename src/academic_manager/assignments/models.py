267ba0130cb2be0e467e9df47eb6ebaf960939d0
nathanquinnkwok <nathanyk@my.yorku.ca>
2025-03-24 13:54:05 -0400

    perf: Added null and blank modifiers
    Added null and blank modifiers to the assignments class

--- src/academic_manager/assignments/models.py
+++ src/academic_manager/assignments/models.py
@@ -5,8 +5,8 @@ from submissions.models import *
 class Assignments(models.Model):
     # Store the variables for this class
     title = models.CharField(max_length=255) # Name of Assignment
-    description = models.TextField() # Description of Assignment
-    due_date = models.DateTimeField() # Store the due date
+    description = models.TextField(null=True, blank=True) # Description of Assignment
+    due_date = models.DateTimeField(null=True, blank=True) # Store the due date
     submissions = models.ManyToManyField(Submissions, related_name="assignments") # Store the submissions, delete submissions once model is deleted
 
     def __str__(self):
